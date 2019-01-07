9cc7a52ba072396674ad5fbdf820efe2996549bc
chafiaifrene <chafiaifrene@outlook.fr>
2019-01-07 22:42:27 +0100

    last commit

--- controler.js
+++ controler.js
@@ -6,32 +6,53 @@ module.exports = (app)=>{
   const { check, validationResult } = require('express-validator/check');
   const bcrypt       = require('bcrypt');
   const saltRounds   = 10;
+  // Permet l'authorisation d'accés a des pages
   const passport     = require('passport');
   const connect      = require('./model/user');
+  const http = require('http');
+  const server = http.Server(app);
+  const io = require('socket.io')(server);
+
+
+  //pour faire une connexion avec la base de données
   const con = connect.connection;
 
+//les Router
+// Page de depart
   app.get('/', (req, res) =>{
     res.render('./login');
   });
-
+// page d'enregistrement
   app.get('/register', (req, res) =>{
     res.render('./register');
   })
+// Page de chat restreinte avec authenticationMiddleware
 
   app.get('/chatRome',authenticationMiddleware (), (req, res) =>{
     res.render('./chatroom');
   })
+
+  //Deconnexion avec suppression de la session
  app.get('/logout', (req, res)=>{
    req.logout();
    req.session.destroy();
    res.redirect('/');
  })
+
+ //Gestion des formulaires
+
+ // utilisation de passport.authorisate pour sois accedé au chat sois /
   app.post('/login', passport.authenticate(
     'local',{
     successRedirect: "/chatRome",
-    failureRedirect: "/"
+    failureRedirect: "/",
+    failureFlash: "invalid password or username "
   }
-  ))
+), function(req, res){
+  req.flash(error)
+});
+
+  // verification des champs de saisie
   app.post('/register', [
 
       check('Name','Nom invalide').isLength({ min: 5 }),
@@ -39,25 +60,27 @@ module.exports = (app)=>{
       check('Password','Password invalide').isLength({ min: 5 })
 
   ], (req, res) => {
-
+    // Variables qui contienent les valeurs des champs
     const Nom       = req.body.Name;
     const Email     = req.body.Email;
     const Password  = req.body.Password;
 
-    const errors = validationResult(req);
+    let errors = validationResult(req);
     if (!errors.isEmpty()) {
-      console.log(errors.array());
+       errors = errors.array();
+       req.flash('errors',errors);
+       res.redirect('/register');
+       res.status(422);
     }
     else {
-
+    // Une fois que tout est bon on les insert dans la base de donnée
       bcrypt.hash(Password, saltRounds, function(err, hash) {
        con.query("INSERT INTO utilisateur (nom,  email, motDepasse) VALUES(?,?,?);", [ Nom, Email, hash] ,(err, result) =>{
           if (err) throw err;
-          console.log("1 record inserted");
           con.query("SELECT LAST_INSERT_ID() as user_id", function(err, result){
             if(err) throw err;
             const user_id = result[0];
-            console.log('Result[0] =', user_id);
+            //Cette fonction permet de creer une session
             req.login(user_id, function(err){
               res.redirect('/chatRome');
             });
@@ -79,12 +102,14 @@ module.exports = (app)=>{
       done(null, user_id);
   });
 
+// fonction qui permet de verifier si l'utilisateur est connecter
+//pour accedé a la page de chatRome
   function authenticationMiddleware () {
  	return (req, res, next) => {
- 		console.log(`req.session.passport.user: ${JSON.stringify(req.session.passport)}`);
-
  	    if (req.isAuthenticated()) return next();
- 	    res.redirect('/')
+ 	    res.redirect('/');
  	}
  }
+ 
+
 }
--- index.js
+++ index.js
@@ -2,34 +2,47 @@
 
 const express  = require('express');
 const app      = express();
+const http = require('http');
+const server = http.Server(app);
+const io = require('socket.io')(server);
 const bp       = require('body-parser');
-const connect      = require('./model/user');
-const con = connect.connection;
+const connect  = require('./model/user');
+const con      = connect.connection;
 const controle = require('./controler');
 const session  = require('express-session');
+const flash    = require('express-flash');
+const port     = process.env.PORT || 3000;
+// Session sur la base de données
 const MySQLStore = require('express-mysql-session')(session);
 const passport     = require('passport');
-const bcrypt       = require('bcrypt');
 const LocalStrategy = require('passport-local').Strategy;
+const bcrypt       = require('bcrypt');
 
-const port     = process.env.PORT || 3000;
+//verification sur une base de données locale
 
 
-//let personne = user.CreateUser("Moad123","jugu@joe.com","123");
-//user.IsMatch("Moad123","123");
 
+//Template pour les views
 app.set('view engine', 'ejs');
+
+//Body parser pour pouvoir manipuler les corps des repenses
 app.use(bp());
+
+//le dossier public contient des fichier statique Html Javascript Css
 app.use(express.static('public'));
 
+// Pour la base données
 const options = {
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "Ichat"
+  host     : "localhost",
+  user     : "root",
+  password : "",
+  database : "Ichat"
 };
 
+// Creation d'une connexion pour la table session
 const sessionStore = new MySQLStore(options);
+
+//Creation d'une session a chaque connexion
 app.use(session({
   secret: 'azertishgfjifgd',
   resave: false,
@@ -37,17 +50,21 @@ app.use(session({
   saveUninitialized: false,
 //  cookie: { secure: true }
 }));
+
+app.use(flash());
 app.use(passport.initialize());
 app.use(passport.session());
+// verification du nom mot de passe sur la base de donnée
 
 passport.use(new LocalStrategy(
-  (username, password, done) =>{
-    console.log('username = '+ username);
-    console.log('password'+ password);
+  (username, password, done)  =>{
+
     con.query("SELECT id, motDepasse FROM utilisateur WHERE nom = ?",[username],(err, res) =>{
      if(err) throw err;
+
+     // utilisateur existe pas
      if(res.length === 0){
-      done(null,false);}
+      done(null,false,{ message: 'Invalid username '});}
      else{
        console.log("utisilateur existe et le mp = " + res[0].motDepasse);
        bcrypt.compare(password,res[0].motDepasse, (err, match)=>{
@@ -66,8 +83,15 @@ passport.use(new LocalStrategy(
   }
 ));
 
+io.on('connection', (socket)=>{
+  console.log(' user Is Connected ... ');
+  socket.on('chat', (data)=>{
+    console.log('Message : ' + data.message);
+    io.sockets.emit('chat', data);
+  })
+})
 controle(app);
 
-app.listen(port, ()=>{
+server.listen(port, ()=>{
 console.log("Server Started on port " + port);
 });
